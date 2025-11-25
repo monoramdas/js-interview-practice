7b26530fcdb32f71061d6eb73d7c6a1aab1f7018
Monoram <monoramdas99@gmail.com>
2025-11-25 23:45:03 +0530

    interviewQuestions: promise section pushed

--- InterviewQuestions.js
+++ InterviewQuestions.js
@@ -52,3 +52,64 @@ console.log(
   "\nFlatten Array=>>",
   ConvertToFlat(array)
 );
+
+// ===========================================================
+
+// 3. Promise Output based Questions
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      if (true) {
+          resolve("done !!")
+      }
+      reject('failed')
+ }, 3000)
+})
+console.log("p1",p1)
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      if (true) {
+          resolve("done !!")
+      }
+      reject('failed')
+ }, 2000)
+})
+console.log("p2",p2)
+
+
+console.log("1. Synchronous log");
+setTimeout(() => {
+  console.log("5. setTimeout with 0ms");
+}, 0);
+Promise.resolve().then(() => {
+  console.log("3. First Promise then");
+
+  Promise.resolve().then(()=>{
+    console.log("nested promise")
+  })
+});
+const promise= new Promise((resolved,reject)=>{
+  console.log("inside promise constructor")
+  resolved(2)
+  let promise =new Promise((res,rej)=>{
+    res(4)
+  })
+  promise.then(val=>{
+    console.log("val",val)
+  })
+})
+promise.then((value)=>{
+  console.log("promise fullfill",value);
+})
+
+
+// async function asyncFunc() {
+//   console.log("2. Inside async function");
+//   await Promise.resolve().then(()=>{
+//     console.log("promise inside async function")
+//   });
+//   console.log("4. After await inside async function");
+// }
+// asyncFunc();
+console.log("6. After async function call");
+
